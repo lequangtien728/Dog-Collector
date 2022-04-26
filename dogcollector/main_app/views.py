4b6542d4358ebec90ad0dec4e8deb362886b35eb
username <youremail@domain.com>
2022-04-25 21:07:21 -0400

    Updating detail url, view, and templates

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Dog
+
+
 
 #Define the home view
 def home(request):
     return HttpResponse('<h1>Tien Quang Le</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def dogs_index(request):
+    dogs = Dog.objects.all()
+    return render(request, 'dogs/index.html', {'dogs': dogs})
+
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render (request, 'dogs/detail.html', {'dog': dog})
