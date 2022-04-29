2f99e3c6e8f757adefdb0b0ad2e9b3fd069f6342
username <youremail@domain.com>
2022-04-28 22:39:22 -0400

    Adding URLs for Authentication in Dog urls.py

--- dogcollector/dogcollector/urls.py
+++ dogcollector/dogcollector/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('main_app.urls')),
+
+    # include the built-in auth urls for the built-in views
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
